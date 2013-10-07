38cac87f8ea33cb7e925bd791b79d96528a7b694
ProPercivalalb <propercivalalb@gmail.com>
2013-10-07 07:58:57 +0100

    Fix for mcmod.info file

--- common/doggytalents/lib/Reference.java
+++ common/doggytalents/lib/Reference.java
@@ -11,12 +11,12 @@ public class Reference {
 
 	//Mod Related Constants 
 	public static final String 		 MOD_ID 	     = "DoggyTalents";
-	public static final String		 MOD_NAME        = "DoggyTalents";
+	public static final String		 MOD_NAME        = "Doggy Talents";
 	public static final String 		 MOD_VERSION     = "v1.5.0a";
 	public static final String 		 MOD_DEPENDENCIES = "required-after:Forge@[9.10.1.850,)";
 	public static final String 		 SP_CLIENT 		 = "doggytalents.core.proxy.ClientProxy";
 	public static final String 		 SP_SERVER		 = "doggytalents.core.proxy.CommonProxy";
-	public static final String       CHANNEL_NAME    = "MAP|MAKING|TOOLS";
+	public static final String       CHANNEL_NAME    = "DOGGY";
 	
 	public static final boolean		 DEBUG 			 = false;
 }

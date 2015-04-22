54feec236180b46702612aaeff2304e2319968fd
ProPercivalalb <propercivalalb@gmail.com>
2015-04-22 20:13:53 +0100

    Deleted old files ready for 1.6.4 downgrade

--- 1.6.4-Code/common/codechicken/nei/api/API.java
+++
@@ -1,9 +0,0 @@
-package codechicken.nei.api;
-
-public class API {
-
-	public static void registerNEIGuiHandler(INEIGuiHandler neiGuiHandler) {
-		
-	}
-
-}
--- 1.6.4-Code/common/doggytalents/api/DoggyEvent.java
+++
@@ -1,9 +0,0 @@
-package doggytalents.api;
-
-/**
- * @author ProPercivalalb
- */
-public interface DoggyEvent {
-
-	//public 
-}

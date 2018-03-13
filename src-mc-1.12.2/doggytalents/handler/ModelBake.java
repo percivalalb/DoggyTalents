734ad4ae3ef3e6d055c871b9a9fb019d9fe38379
ProPercivalalb <propercivalalb@gmail.com>
2018-03-13 14:55:19 +0000

    Fix dog bed render

--- src-mc-1.12.2/doggytalents/handler/ModelBake.java
+++ src-mc-1.12.2/doggytalents/handler/ModelBake.java
@@ -2,6 +2,7 @@ package doggytalents.handler;
 
 import doggytalents.DoggyTalents;
 import doggytalents.client.model.block.DogBedModel;
+import doggytalents.lib.Reference;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
@@ -9,8 +10,10 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@EventBusSubscriber(modid = Reference.MOD_ID)
 public class ModelBake {
 
 	@SubscribeEvent

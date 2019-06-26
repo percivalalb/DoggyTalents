8496fd5ed0ee9bb4ff9c15eea18aa88464809668
ProPercivalalb <propercivalalb@gmail.com>
2019-06-26 20:05:36 +0100

    [1.14] Improve dog bed model overriding

--- src-mc-1.14.x/doggytalents/handler/ModelBake.java
+++ src-mc-1.14.x/doggytalents/handler/ModelBake.java
@@ -1,14 +1,15 @@
 package doggytalents.handler;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map.Entry;
+import java.util.Map;
 
 import doggytalents.DoggyTalentsMod;
+import doggytalents.ModBlocks;
 import doggytalents.client.model.block.DogBedModel;
 import doggytalents.lib.Reference;
+import net.minecraft.client.renderer.BlockModelShapes;
 import net.minecraft.client.renderer.model.BlockModel;
 import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
@@ -18,29 +19,30 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.model.TRSRTransformation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModelBake {
 
     @SubscribeEvent
     public static void onModelBakeEvent(final ModelBakeEvent event) {
+        Map<ResourceLocation, IBakedModel> modelRegistry = event.getModelRegistry();
+        
         try {
-            DoggyTalentsMod.LOGGER.debug("Replacing dog bed model.");
-            BlockModel model = (BlockModel)event.getModelLoader().getUnbakedModel(new ResourceLocation(Reference.MOD_ID, "block/dog_bed"));
+            ResourceLocation resourceLocation = ForgeRegistries.BLOCKS.getKey(ModBlocks.DOG_BED);
+            ResourceLocation unbakedModelLoc = new ResourceLocation(resourceLocation.getNamespace(), "block/" + resourceLocation.getPath());
+            
+            BlockModel model = (BlockModel)event.getModelLoader().getUnbakedModel(unbakedModelLoc);
             IBakedModel customModel = new DogBedModel(event.getModelLoader(), model, model.bake(event.getModelLoader(), ModelLoader.defaultTextureGetter(), TRSRTransformation.getRotation(Direction.NORTH), DefaultVertexFormats.BLOCK), DefaultVertexFormats.BLOCK);
 
-            List<ResourceLocation> modelsToReplace = new ArrayList<ResourceLocation>();
-                
-            for(Entry<ResourceLocation, IBakedModel> entry : event.getModelRegistry().entrySet()) {
-                if(entry.getKey().getNamespace().equals(Reference.MOD_ID) && entry.getKey().getPath().equals("dog_bed")) {
-                    modelsToReplace.add(entry.getKey());
-                }
-            }
-
-            for(ResourceLocation location : modelsToReplace) {
-                //Replace 
-                event.getModelRegistry().put(location, customModel);
-            }
+            // Replace all valid block states
+            ModBlocks.DOG_BED.getStateContainer().getValidStates().forEach(state -> {
+                modelRegistry.put(BlockModelShapes.getModelLocation(state), customModel);
+            });
+            
+            // Replace inventory model
+            modelRegistry.put(new ModelResourceLocation(resourceLocation, "inventory"), customModel);
+            
         }
         catch(Exception e) {
             DoggyTalentsMod.LOGGER.warn("Could not get base Dog Bed model. Reverting to default textures...");

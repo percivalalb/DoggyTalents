1126be2206c4fb430dd95ef6726455ab60b195cf
Alex Barter <alex.leo.barter@gmail.com>
2020-02-11 11:51:43 +0000

    Improve throw render and use MatrixStack

--- src/main/java/doggytalents/client/renderer/entity/layer/LayerBone.java
+++ src/main/java/doggytalents/client/renderer/entity/layer/LayerBone.java
@@ -9,6 +9,8 @@ import doggytalents.client.renderer.entity.RenderDog;
 import doggytalents.entity.EntityDog;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Quaternion;
+import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.OverlayTexture;
@@ -26,26 +28,26 @@ public class LayerBone extends LayerRenderer<EntityDog, ModelDog> {
     public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, EntityDog dog, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         if(dog.hasBone()) {
 
-            RenderSystem.pushMatrix();
+            matrixStackIn.push();
             RenderSystem.enableLighting();
             if(this.getEntityModel().isChild) { //isChild
                 float f = 0.5F;
-                RenderSystem.translatef(0.0F, 0.75F, -0.25F);
-                RenderSystem.scalef(0.5F, 0.5F, 0.5F);
+                matrixStackIn.translate(0.0F, 0.75F, -0.25F);
+                matrixStackIn.scale(0.5F, 0.5F, 0.5F);
             }
 
             if(dog.isShiftKeyDown())
-                RenderSystem.translatef(0.0F, 0.2F, 0.0F);
+                matrixStackIn.translate(0.0F, 0.2F, 0.0F);
+           // matrixStackIn.rotate(Vector3f.ZP.rotation((float) (Math.PI / 4)));
+            this.getEntityModel().head.setAnglesAndRotation(matrixStackIn);
+           // matrixStackIn.rotate(new Quaternion(90.0F, 0.0F, 1.0F, 0.0F));
+           /// matrixStackIn.rotate(new Quaternion(90.0F, 1.0F, 0.0F, 0.0F));
+           // matrixStackIn.rotate(new Quaternion(45.0F, 0.0F, 0.0F, 1.0F));
 
-            //this.getEntityModel().head.postRender(0.0625F);
-            RenderSystem.rotatef(90.0F, 0.0F, 1.0F, 0.0F);
-            RenderSystem.rotatef(90.0F, 1.0F, 0.0F, 0.0F);
-            RenderSystem.rotatef(45.0F, 0.0F, 0.0F, 1.0F);
-
-            RenderSystem.translated(0.20, -0.10, -0.10);
+            matrixStackIn.translate(0.00, 0.0, -0.3);
             IThrowableItem throwableItem = dog.getThrowableItem();
-            RenderSystem.popMatrix();
             Minecraft.getInstance().getItemRenderer().renderItem(throwableItem != null ? throwableItem.getRenderStack(dog.getBoneVariant()) : dog.getBoneVariant(), ItemCameraTransforms.TransformType.GROUND, packedLightIn, OverlayTexture.DEFAULT_LIGHT, matrixStackIn, bufferIn);
+            matrixStackIn.pop();
         }
     }
 }

c4f9d7a9d163eddb5078ccedf6f4c86d8b88b7f6
Alex Barter <alex.leo.barter@gmail.com>
2020-02-22 17:15:29 +0000

    Match 1.15.x bone render transformations

--- src/main/java/doggytalents/client/renderer/entity/layer/LayerBone.java
+++ src/main/java/doggytalents/client/renderer/entity/layer/LayerBone.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class LayerBone implements LayerRenderer<EntityDog> {
-    
+
     protected final RenderDog dogRenderer;
     public LayerBone(RenderDog dogRendererIn) {
         this.dogRenderer = dogRendererIn;
@@ -22,30 +22,26 @@ public class LayerBone implements LayerRenderer<EntityDog> {
     @Override
     public void doRenderLayer(EntityDog dog, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
         if(dog.hasBone()) {
-            
+
             GlStateManager.pushMatrix();
-    
+
             if (this.dogRenderer.getMainModel().isChild) {
-                float f = 0.5F;
-                GlStateManager.translate(0.0F, 0.75F, -0.25F);
-                GlStateManager.scale(0.5F, 0.5F, 0.5F);
+                // derived from AgeableModel head offset
+                GlStateManager.translate(0.0F, 5.0F / 16.0F, 2.0F / 16.0F);
             }
 
-            if(dog.isSneaking())
-                GlStateManager.translate(0.0F, 0.2F, 0.0F);
-
             ((ModelDog)this.dogRenderer.getMainModel()).wolfHeadMain.postRender(0.0625F);
-            GlStateManager.rotate(90.0F, 0.0F, 1.0F, 0.0F);
-            GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
-            GlStateManager.rotate(45.0F, 0.0F, 0.0F, 1.0F);
 
-            GlStateManager.translate(0.20, -0.10, -0.10);
+            GlStateManager.translate(-0.025F, 0.125F, -0.32F);
+            GlStateManager.rotate(45.0F, 0.0F, 1.0F, 0.0F);
+            GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
+
             IThrowableItem throwableItem = dog.getThrowableItem();
-            Minecraft.getMinecraft().getItemRenderer().renderItem(dog, throwableItem != null ? throwableItem.getRenderStack(dog.getBoneVariant()) : dog.getBoneVariant(), ItemCameraTransforms.TransformType.NONE);
+            Minecraft.getMinecraft().getItemRenderer().renderItemSide(dog, throwableItem != null ? throwableItem.getRenderStack(dog.getBoneVariant()) : dog.getBoneVariant(), ItemCameraTransforms.TransformType.GROUND, false);
             GlStateManager.popMatrix();
         }
     }
-    
+
     @Override
     public boolean shouldCombineTextures() {
         return false;

a4c33b16ed907b5d8e4a042a8789cfe4e1a79b52
Alex Barter <alex.leo.barter@gmail.com>
2020-05-27 20:32:33 +0100

    Add constants for ItemPhysics comp

---
+++ src/main/java/doggytalents/addon/itemphysic/ItemPhysicsAddon.java
@@ -0,0 +1,18 @@
+package doggytalents.addon.itemphysic;
+
+public class ItemPhysicsAddon {
+
+    private static final String className = "team.creative.itemphysic.api.ItemPhysicAPI";
+
+    private static final String methodName = "addSortingObjects";
+
+    private static final String swimmingItems = "swimmingItems";
+    private static final String burningItems = "burningItems";
+    private static final String undestroyableItems = "undestroyableItems";
+    private static final String ignitingItems = "ignitingItems";
+
+    public static void init() {
+
+    }
+
+}

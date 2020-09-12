0fdfb88454c4d693f9f8f42f565e3a2533851808
Alex Barter <alex.leo.barter@gmail.com>
2020-09-12 09:45:30 +0100

    Finish creeper sweeper talent

--- src/main/java/doggytalents/common/talent/CreeperSweeperTalent.java
+++ src/main/java/doggytalents/common/talent/CreeperSweeperTalent.java
@@ -1,5 +1,7 @@
 package doggytalents.common.talent;
 
+import java.util.List;
+
 import doggytalents.api.feature.DataKey;
 import doggytalents.api.inferface.AbstractDogEntity;
 import doggytalents.api.registry.Talent;
@@ -7,6 +9,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.SoundEvents;
 
 public class CreeperSweeperTalent extends Talent {
 
@@ -19,21 +22,18 @@ public class CreeperSweeperTalent extends Talent {
 
     @Override
     public void tick(AbstractDogEntity dogIn) {
-//        int level = dogIn.getLevel(this);
-//
-//        if(dogIn.getAttackTarget() == null && dogIn.isTamed() && level > 0) {
-//            List<CreeperEntity> list = dogIn.world.getEntitiesWithinAABB(CreeperEntity.class, dogIn.getBoundingBox().grow(level * 5, level * 2, level * 5));
-//
-//            if(!list.isEmpty() && !dogIn.isSitting() && dogIn.getHealth() > 1) {
-//                int timeLeft = dogIn.getDataOrDefault(COOLDOWN, () -> dogIn.ticksExisted) - dogIn.ticksExisted;
-//                dogIn.putObject("creeper_timer", timer - 1);
-//            }
-//        }
-//
-//        if(dogIn.getObject("creeper_timer", Integer.TYPE) < 0) {
-//            dogIn.playSound(SoundEvents.ENTITY_WOLF_GROWL, dogIn.getSoundVolume(), (dogIn.getRNG().nextFloat() - dogIn.getRNG().nextFloat()) * 0.2F + 1.0F);
-//            dogIn.putObject("creeper_timer", 60 + dogIn.getRNG().nextInt(40));
-//        }
+        int level = dogIn.getLevel(this);
+
+        int timeLeft = dogIn.getDataOrGet(COOLDOWN, () -> dogIn.ticksExisted) - dogIn.ticksExisted;
+
+        if(timeLeft <= 0 && !dogIn.isSitting()) {
+            List<CreeperEntity> list = dogIn.world.getEntitiesWithinAABB(CreeperEntity.class, dogIn.getBoundingBox().grow(level * 5, level * 2, level * 5));
+
+            if(!list.isEmpty()) {
+                dogIn.playSound(SoundEvents.ENTITY_WOLF_GROWL, dogIn.getSoundVolume(), (dogIn.getRNG().nextFloat() - dogIn.getRNG().nextFloat()) * 0.2F + 1.0F);
+                dogIn.setData(COOLDOWN, dogIn.ticksExisted + 60 + dogIn.getRNG().nextInt(40));
+            }
+        }
 
         if(dogIn.getAttackTarget() instanceof CreeperEntity) {
             CreeperEntity creeper = (CreeperEntity) dogIn.getAttackTarget();

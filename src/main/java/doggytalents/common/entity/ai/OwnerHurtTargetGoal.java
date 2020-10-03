a51b3d9ef62dca0fbc338b2418cf6135dcfbb5ca
Alex Barter <alex.leo.barter@gmail.com>
2020-10-03 12:48:48 +0100

    Add missing class file

---
+++ src/main/java/doggytalents/common/entity/ai/OwnerHurtTargetGoal.java
@@ -0,0 +1,19 @@
+package doggytalents.common.entity.ai;
+
+import doggytalents.api.feature.EnumMode;
+import doggytalents.common.entity.DogEntity;
+
+public class OwnerHurtTargetGoal extends net.minecraft.entity.ai.goal.OwnerHurtTargetGoal {
+
+    private DogEntity dog;
+
+    public OwnerHurtTargetGoal(DogEntity dogIn) {
+        super(dogIn);
+        this.dog = dogIn;
+    }
+
+    @Override
+    public boolean shouldExecute() {
+         return this.dog.isMode(EnumMode.AGGRESIVE, EnumMode.BERSERKER, EnumMode.TACTICAL) && super.shouldExecute();
+    }
+}

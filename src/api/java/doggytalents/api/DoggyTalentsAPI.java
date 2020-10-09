ea36ed9efb113125b5a8e8e60c0a6a7c62c8205c
Alex Barter <alex.leo.barter@gmail.com>
2020-10-09 07:30:33 +0100

    Fix compile issues

--- src/api/java/doggytalents/api/DoggyTalentsAPI.java
+++ src/api/java/doggytalents/api/DoggyTalentsAPI.java
@@ -1,5 +1,8 @@
 package doggytalents.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import doggytalents.api.registry.Accessory;
 import doggytalents.api.registry.AccessoryType;
 import doggytalents.api.registry.IBeddingMaterial;
@@ -17,4 +20,6 @@ public class DoggyTalentsAPI {
     public static IForgeRegistry<AccessoryType> ACCESSORY_TYPE;
     public static IForgeRegistry<IBeddingMaterial> BEDDING_MATERIAL;
     public static IForgeRegistry<ICasingMaterial> CASING_MATERIAL;
+
+    public static final Logger LOGGER = LogManager.getLogger("doggytalents");
 }

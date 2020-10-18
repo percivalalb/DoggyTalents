4b3817a2fd9a01359b632acfbc232de1278ec9f6
Alex Barter <alex.leo.barter@gmail.com>
2020-10-18 21:14:27 +0100

    Fix Botania dog beds missing texture (#261)

--- src/main/java/doggytalents/common/addon/botania/BotaniaAddon.java
+++ src/main/java/doggytalents/common/addon/botania/BotaniaAddon.java
@@ -24,21 +24,15 @@ public class BotaniaAddon implements Addon {
     public static final String[] BLOCKS = {"livingwood_planks", "mossy_livingwood_planks",
             "dreamwood_planks", "mossy_dreamwood_planks", "shimmerwood_planks"};
 
-    public static final String[] TEXTURES = {"livingwood1", "livingwood2",
-            "dreamwood1", "dreamwood2", "shimmerwoodplanks"};
-
     public final void registerCasings(final RegistryEvent.Register<ICasingMaterial> event) {
         if (!this.shouldLoad()) { return; }
         IForgeRegistry<ICasingMaterial> casingRegistry = event.getRegistry();
 
-        int i = 0;
         for (String block : BLOCKS) {
             ResourceLocation rl = Util.getResource(MOD_ID, block);
-            ResourceLocation texture = Util.getResource(MOD_ID, "blocks/" + TEXTURES[i]);
             Supplier<Block> blockGet = () -> ForgeRegistries.BLOCKS.getValue(rl);
 
-            casingRegistry.register(new CasingMaterial(blockGet, texture).setRegistryName(rl));
-            i++;
+            casingRegistry.register(new CasingMaterial(blockGet).setRegistryName(rl));
         }
     }
 

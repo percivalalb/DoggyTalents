43a59926ed0ace887f00743733093f020d086ca8
Alex Barter <alex.leo.barter@gmail.com>
2020-10-31 08:37:14 +0000

    Fix possible NPE #265

--- src/main/java/doggytalents/common/talent/HunterDogTalent.java
+++ src/main/java/doggytalents/common/talent/HunterDogTalent.java
@@ -3,20 +3,25 @@ package doggytalents.common.talent;
 import doggytalents.DoggyTalents;
 import doggytalents.common.entity.DogEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LootingLevelEvent;
 
 public class HunterDogTalent {
 
     public static void onLootDrop(final LootingLevelEvent event) {
-        Entity trueSource = event.getDamageSource().getTrueSource();
-        if (trueSource instanceof DogEntity) {
-            DogEntity dog = (DogEntity) trueSource;
-            int level = dog.getLevel(DoggyTalents.HUNTER_DOG);
+        DamageSource damageSource = event.getDamageSource();
 
-            if (dog.getRNG().nextInt(6) < level + (level >= 5 ? 1 : 0)) {
-                event.setLootingLevel(event.getLootingLevel() + level / 2);
+        // Possible to be null #265
+        if (damageSource != null) {
+            Entity trueSource = damageSource.getTrueSource();
+            if (trueSource instanceof DogEntity) {
+                DogEntity dog = (DogEntity) trueSource;
+                int level = dog.getLevel(DoggyTalents.HUNTER_DOG);
+
+                if (dog.getRNG().nextInt(6) < level + (level >= 5 ? 1 : 0)) {
+                    event.setLootingLevel(event.getLootingLevel() + level / 2);
+                }
             }
-
         }
     }
 }

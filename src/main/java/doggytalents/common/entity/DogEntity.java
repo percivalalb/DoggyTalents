145a1b741834fa56a860dc127b9c88e84b42ae6f
DashieDev <61554666+DashieDev@users.noreply.github.com>
2021-11-25 23:52:44 +0700

    Restrict ability to leash dogs to allowed player
    Closes #368

--- src/main/java/doggytalents/common/entity/DogEntity.java
+++ src/main/java/doggytalents/common/entity/DogEntity.java
@@ -908,6 +908,11 @@ public class DogEntity extends AbstractDogEntity {
     }
 
     @Override
+    public boolean canBeLeashed(Player player) {
+        return this.canInteract(player) && super.canBeLeashed(player);
+    }
+
+    @Override
     public void setUUID(UUID uniqueIdIn) {
 
         // If the UUID is changed remove old one and add new one

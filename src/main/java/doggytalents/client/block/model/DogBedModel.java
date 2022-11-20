be40860750071974a1ac2a3be7b61087f18f6fbe
percivalalb <alex.leo.barter@gmail.com>
2022-11-20 19:53:21 +0000

    Make dog bed render cache thread-safe

--- src/main/java/doggytalents/client/block/model/DogBedModel.java
+++ src/main/java/doggytalents/client/block/model/DogBedModel.java
@@ -41,7 +41,7 @@ public class DogBedModel implements BakedModel {
     private BlockModel model;
     private BakedModel bakedModel;
 
-    private final Map<Triple<ICasingMaterial, IBeddingMaterial, Direction>, BakedModel> cache = Maps.newHashMap();
+    private final Map<Triple<ICasingMaterial, IBeddingMaterial, Direction>, BakedModel> cache = Maps.newConcurrentMap();
 
     public DogBedModel(ModelBakery modelLoader, BlockModel model, BakedModel bakedModel) {
         this.modelLoader = modelLoader;
